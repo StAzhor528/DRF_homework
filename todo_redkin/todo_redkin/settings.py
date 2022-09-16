6f4411ac3f2ff62a96cefe723bce6055db7581fb
Ivan <stazhor@bk.ru>
2022-09-16 19:57:37 +0300

    docker

--- todo_redkin/todo_redkin/settings.py
+++ todo_redkin/todo_redkin/settings.py
@@ -29,7 +29,9 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
+    'http://0.0.0.0:80',
 ]
+CORS_ALLOW_All_ORIGINS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -85,10 +87,21 @@ WSGI_APPLICATION = 'todo_redkin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'PASSWORD': '1',
+        'USER': 'django',
+        'HOST': 'db',
+        'port': '5432',
     }
 }
 
